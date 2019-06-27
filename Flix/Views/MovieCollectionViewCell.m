13b176440e3636794da6b0a4e258ad7efaff30ea
Yuewei Yuan <lucyyyw@fb.com>
2019-06-27 14:23:30 -0700

    collection view setup

---
+++ Flix/Views/MovieCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.m
+//  Flix
+//
+//  Created by lucyyyw on 6/27/19.
+//  Copyright © 2019 lucyyyw. All rights reserved.
+//
+
+#import "MovieCollectionViewCell.h"
+
+@implementation MovieCollectionViewCell
+
+@end
